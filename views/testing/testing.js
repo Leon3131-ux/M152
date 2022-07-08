044337aa60905a02ff2850a24cab9d34d3c01126
leon3131-ux <knud.del@hotmail.com>
2022-07-08 14:12:13 +0200

    stuff

---
+++ views/testing/testing.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $("#navbar").load("../../common/navigation/navbar.html", function () {
+        $("#testingLink").attr("aria-current", "true").addClass("active");
+    });
+});
+function updateText(){
+
+}
